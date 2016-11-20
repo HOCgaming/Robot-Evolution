52bcdb69d92505c93fc396fc83ab0d616eb0edcf
Unknown <vbondarenko1@sheffield.ac.uk>
2016-11-20 01:34:42 +0000

    God...

--- Robot Evolution/Assets/Scripts/CraftinPillars.cs
+++ Robot Evolution/Assets/Scripts/CraftinPillars.cs
@@ -16,7 +16,7 @@ public class CraftinPillars : MonoBehaviour {
 
 	public void pillarUp(float x){
 		
-		if ((gameObject.transform.position.y < 0.842f) && (gameObject.transform.position.y >= 0f)){
+		if ((gameObject.transform.position.y < 0.674f) && (gameObject.transform.position.y >= 0f)){
 			transform.Translate (0f, x, 0f);
 		}
 	}
