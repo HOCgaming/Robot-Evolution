60430e14de8a9844f19c5f2a82eb7f826b7cedef
Unknown <vbondarenko1@sheffield.ac.uk>
2016-11-20 02:29:05 +0000

    Boooom!

--- Robot Evolution/Assets/Scripts/Platform2.cs
+++ Robot Evolution/Assets/Scripts/Platform2.cs
@@ -13,11 +13,14 @@ public class Platform2 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (button.Activate ());
-		if (gameObject.transform.position.y >= 0.43f)
+		
+		if (gameObject.transform.position.y >= 0.43f) {
 			MaxHeight = true;
-		if ((button.Activate() == 2) && (!MaxHeight))
+		}
+		if ((button.Activate () == 2) && (!MaxHeight)) {
+			Debug.Log (button.Activate ());
 			transform.Translate (0f, 0.005f, 0f);
+		}
 		if (MaxHeight && gameObject.transform.position.y >= 0.035) {
 			transform.Translate (0f, -0.01f, 0f);
 		}
