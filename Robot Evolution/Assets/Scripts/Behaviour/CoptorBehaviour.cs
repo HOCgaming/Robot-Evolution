970bd7ce6e1d2e3e0772c2fe1ea604f485101422
Unknown <jamieapps101@gmail.com>
2016-11-20 15:55:24 +0000

    Playable Game!
    Kinda!

---
+++ Robot Evolution/Assets/Scripts/Behaviour/CoptorBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoptorBehaviour : MonoBehaviour {
+
+	private ComponentClass myComponent;
+	private int forceFactor, spinFactor;
+
+	void Start () {
+		forceFactor = 10;
+		spinFactor = 10;
+		myComponent = gameObject.GetComponent<ComponentClass>();
+	}
+
+	void FixedUpdate () {
+
+		if (myComponent.isAttachedToSomething && Input.GetKey(KeyCode.Space))
+		{
+			//gameObject.transform.Rotate(rotateAxis, Time.deltaTime * wheelFactor);
+			GlobalReferences.RobotCentre.GetComponent<Rigidbody>().AddForce(Vector3.up * forceFactor);
+			gameObject.transform.RotateAround (gameObject.transform.up, Time.deltaTime * spinFactor);
+		}
+
+
+	}
+}
